70a79af02964f5e50d4f6a77d3f97ca1703f8ba2
Patryk <pat-hur@wp.pl>
2024-08-11 16:17:21 +0200

    a couple fixes of server.js and docker

--- server/server.js
+++ server/server.js
@@ -2,18 +2,19 @@
 require('dotenv').config();
 
 const express = require('express');
+const cors = require('cors');
 const { connectToDatabase } = require('./db');
 const routes = require('./routes');
 
 const app = express();
-const port = process.env.PORT || 13000; 
+const port = process.env.PORT; 
 
 app.use(express.json());
-
+app.use(cors());
 app.use(routes);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('No witam cześć!');
 });
 
 connectToDatabase().then(() => {
